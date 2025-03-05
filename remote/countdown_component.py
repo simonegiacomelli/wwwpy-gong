1896db0f40cdd5eeb5d5e39ea3753a110d11d25b
simone <simone.giacomelli@gmail.com>
2025-03-05 09:16:24 +0000

    refactor countdown component for improved readability and functionality

--- remote/countdown_component.py
+++ remote/countdown_component.py
@@ -1,9 +1,9 @@
-import js
+import asyncio
 import inspect
 import logging
 import time
-import asyncio
 
+import js
 import wwwpy.remote.component as wpc
 
 logger = logging.getLogger(__name__)
@@ -16,7 +16,6 @@ class CountdownComponent(wpc.Component, tag_name='u-countdown'):
     _countdown: js.HTMLDivElement = wpc.element()
 
     def init_component(self):
-        self.cycle_time = 60
         self.start_time = None
         self.task = None
         self.element.innerHTML = """
@@ -28,43 +27,59 @@ class CountdownComponent(wpc.Component, tag_name='u-countdown'):
             <br>
             <div data-name="_countdown">#</div>
         """
-        self.reset_ui()
+        self.cycle_time = 60
+        self._reset_ui()
 
-    def format_time(self, seconds):
+    @property
+    def cycle_time(self) -> int:
+        return self._cycle_time
+
+    @cycle_time.setter
+    def cycle_time(self, value):
+        self._cycle_time = value
+        self.stop()
+
+    def _format_time(self, seconds):
         if seconds < 0 or seconds is None:
             seconds = 0
         minutes = seconds // 60
         remaining_seconds = seconds % 60
         return f"{minutes:02}:{remaining_seconds:02}"
 
-    def update_ui(self, countdown, total_time):
-        self._countdown.innerHTML = f"Cycle end:<br>{self.format_time(countdown)}"
-        self._total_time.innerHTML = f"Total time:<br>{self.format_time(total_time)}"
+    def _update_ui(self, countdown, total_time):
+        self._countdown.innerHTML = f"Cycle end:<br>{self._format_time(countdown)}"
+        self._total_time.innerHTML = f"Total time:<br>{self._format_time(total_time)}"
 
-    async def play_sound(self):
+    async def _play_sound(self):
         js.Audio.new("https://fs.simone.pro/shared/gong/singing-bowl-gong-69238.mp3").play()
 
-    async def timer_tick(self):
+    async def _timer_tick(self):
         while self.start_time:
             elapsed = int(time.time() - self.start_time)
             cycle_used = elapsed % self.cycle_time
             if cycle_used == 0:
-                await self.play_sound()
-            self.update_ui(self.cycle_time - cycle_used, elapsed)
+                await self._play_sound()
+            self._update_ui(self.cycle_time - cycle_used, elapsed)
             await asyncio.sleep(1)
 
     async def _btn_start__click(self, event):
         logger.debug(f'{inspect.currentframe().f_code.co_name} event fired %s', event)
+        await self.start()
+
+    async def start(self):
         if not self.start_time:
             self.start_time = time.time()
-            self.task = asyncio.create_task(self.timer_tick())
+            self.task = asyncio.create_task(self._timer_tick())
 
     async def _btn_stop__click(self, event):
         logger.debug(f'{inspect.currentframe().f_code.co_name} event fired %s', event)
+        await self.stop()
+
+    async def stop(self):
         if self.task:
             self.task.cancel()
         self.start_time = None
-        self.reset_ui()
+        self._reset_ui()
 
-    def reset_ui(self):
-        self.update_ui(self.cycle_time, 0)
+    def _reset_ui(self):
+        self._update_ui(self.cycle_time, 0)
--- remote/main_component.py
+++ remote/main_component.py
@@ -7,11 +7,17 @@ import logging
 logger = logging.getLogger(__name__)
 from remote.countdown_component import CountdownComponent
 
+
 class MainComponent(wpc.Component, tag_name='component-1'):
+    _c1: CountdownComponent = wpc.element()
+    _c2: CountdownComponent = wpc.element()
 
     def init_component(self):
         # language=html
-        self.element.innerHTML = """
-<div>component-1</div>
-<u-countdown></u-countdown>
+        self.element.innerHTML = """<div style="display: flex; gap: 10px">
+    <u-countdown data-name='_c1'></u-countdown>
+    <u-countdown data-name='_c2'></u-countdown>
+</div>
 """
+        self._c1.cycle_time = 20*60
+        self._c2.cycle_time = 20
